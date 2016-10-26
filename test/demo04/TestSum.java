86880281d1741bad65d6366d7ceddace72894498
JoinHearthstone <joinhearthstone@outlook.es>
2016-10-26 18:08:06 +0200

    Soporte para negativos

--- test/demo04/TestSum.java
+++ test/demo04/TestSum.java
@@ -16,6 +16,14 @@ public class TestSum {
 		assertEquals(4, sum(4, 0));
 		assertEquals(4, sum(0, 4));
 	}
+
+	
+	@Test
+	public void testSumNeg() {
+		assertEquals(7, sum(10, -3));
+		assertEquals(7, sum(-3, 10));
+		assertEquals(-8, sum(-4, -4));
+	}
 	
 	private int sum(int i, int j) {
 		return i+j;
