2b504242c6e82b261981f34d6598e2062ac4cdbe
JoinHearthstone <joinhearthstone@outlook.es>
2016-10-26 18:05:36 +0200

    Soporte para cero

--- test/demo04/TestSum.java
+++ test/demo04/TestSum.java
@@ -11,8 +11,14 @@ public class TestSum {
 		assertEquals(6, sum(2,4));
 	}
 
+	@Test
+	public void testSumCero() {
+		assertEquals(4, sum(4, 0));
+		assertEquals(4, sum(0, 4));
+	}
+	
 	private int sum(int i, int j) {
-		return 6;
+		return i+j;
 	}
 
 }
